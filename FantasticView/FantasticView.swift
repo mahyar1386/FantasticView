a560e956bcc30af850b90f50de5b51ff151f51e8
Mahyar <mahyar@MacBook-Pro.local>
2018-03-07 14:27:19 +0330

    init

---
+++ FantasticView/FantasticView.swift
@@ -0,0 +1,13 @@
+//
+//  FantasticView.swift
+//  FantasticView
+//
+//  Created by Mahyar on 3/7/18.
+//  Copyright © 2018 Mahyar. All rights reserved.
+//
+
+import UIKit
+
+class FantasticView: NSObject {
+
+}
